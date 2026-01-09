de20ef0c7497b02bcacb3ff93b03f59e6a603e22
Georgy29 <georgy.blokhin@students.opit.com>
2026-01-09 18:04:09 +0100

    schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,72 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional 
+
+class UserBase(BaseModel):
+    username: str 
+    email: str
+    
+class UserCreate(UserBase):
+    password: str
+    
+class User(UserBase):
+    id: int
+    created_at: datetime
+    
+# allows the model to be initialized using objects
+# or ORM-like attributes intead of just plain dictionaries
+    class Config:
+        from_attributes = True
+        
+# Token schemas
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+# Post schemas
+class PostBase(BaseModel):
+    content: str
+
+class PostCreate(PostBase):
+    pass
+
+class Post(PostBase):
+    id: int
+    timestamp: datetime
+    owner_id: int
+    
+    class Config:
+        from_attributes = True
+        
+class PostWithCounts(Post):
+    likes_count: int
+    retweets_count: int
+    owner_username: str
+    
+class PostUpdate(BaseModel):
+    content: str 
+    
+    class Config:
+        from_attributes = True
+     
+# Like schema
+   
+class Like(BaseModel):
+    user_id: str
+    post_id: str
+    
+    class Config:
+        from_attributes = True
+        
+# Retweet schema 
+class Retweet(BaseModel):
+    user_id: str
+    post_id: str
+    timestamp: datetime
+    
+    class Config:
+        from_attributes = True 

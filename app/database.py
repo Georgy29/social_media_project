cf3cbde8f9d7fb2ff761f344887538ff12c57aa1
Georgy29 <georgy.blokhin@students.opit.com>
2026-01-11 03:22:14 +0100

    update pathing for database

--- app/database.py
+++ app/database.py
@@ -2,15 +2,13 @@ from pathlib import Path
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from .settings import DATABASE_URL
 
 BASE_DIR = Path(__file__).resolve().parent.parent 
 DB_FILE = BASE_DIR / "microblog.db"
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_FILE.as_posix()}"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

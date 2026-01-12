669add7d7a693611e594d4c4a9bf30e283811292
Georgy29 <georgy.blokhin@students.opit.com>
2026-01-12 22:10:25 +0100

    Ruff

--- app/auth.py
+++ app/auth.py
@@ -8,8 +8,8 @@ from jwt import PyJWTError
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 
-from . import settings 
-from . import models, schemas
+from . import settings
+from . import models
 from .database import get_db
 
 SECRET_KEY = settings.SECRET_KEY
@@ -22,11 +22,14 @@ pwd_context = CryptContext(
 )
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
+
 def get_password_hash(password):
     return pwd_context.hash(password)
+
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
@@ -34,6 +37,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
 
 async def get_current_user(
     token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
@@ -45,14 +49,15 @@ async def get_current_user(
     )
 
     try:
-        payload= jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: Optional[str] = payload.get("sub")
         if username is None:
             raise credentials_exception
     except PyJWTError:
         raise credentials_exception
-    user = db.query(models.User).filter(models.User.username == username).first() # maybe wrong check after adding api 
+    user = (
+        db.query(models.User).filter(models.User.username == username).first()
+    )  # maybe wrong check after adding api
     if user is None:
         raise credentials_exception
     return user
-
